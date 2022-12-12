7b703ec72b23cdd29632bee6d82087d9e81dfae7
Kimshii <117647269+Kimshii@users.noreply.github.com>
2022-12-12 13:34:48 +0100

    Update CarFactory.java

--- src/model/CarFactory.java
+++ src/model/CarFactory.java
@@ -1,23 +1,19 @@
 package src.model;
-
 import java.awt.*;
 
 public class CarFactory {
 
-    public static Volvo240 createVolvoCar() {
+    public static Car createVolvo(){
         return new Volvo240(4, 100, 0, Color.black, "Volvo240", 0, 0, false, false);
     }
-
-    public static Saab95 createSaabCar() {
-        return new Saab95(2, 125, 0, Color.red, "Saab95", 0, 0, false, false);
+    public static Car createSaab(){
+        return new Saab95(2, 125, 0, Color.red, "Saab95", 0, 100, false, false);
     }
-
-    public static Scania createScaniaCar() {
-        return new Scania(2, 375, 0, Color.yellow, "Scania", 0, 0, false, false, 0, 70);
+    public static Car createScania(){
+        return new Scania(2, 375, 0, Color.green, "Scania", 0, 200, false, false, 0, 70);
     }
-
-    public static Transport createTransporterCar() {
-        return new Transport(2, 375, 0, Color.blue, "CarTransporter", 0, 0, false, false, 4, 0);
+    public static Car createtransport(){
+        return new Transport(2, 375, 0, Color.green, "Transport", 0, 300, false, false, 0, 0);
     }
+    
 }
-
